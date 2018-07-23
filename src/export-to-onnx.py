fa6700f464817c4192a553ef63176c1d39b2bde8
Vegard Ekblad Itland <Vegard.Itland@student.uib.no>
2018-07-23 10:21:43 +0200

    Fix 'error parsing message' error

--- src/export-to-onnx.py
+++ src/export-to-onnx.py
@@ -1,4 +1,7 @@
 import tensorflow as tf
+from tensorflow.python.util import compat
+from tensorflow.core.protobuf import saved_model_pb2
+
 from model_converters import KerasToTensorflow
 from onnx_tf.frontend import tensorflow_graph_to_onnx_model
 
@@ -7,8 +10,11 @@ KerasToTensorflow.convert("model.h5", "export")
 
 # convert Tensorflow frozen .pb format to ONNX format
 with tf.gfile.GFile("export/saved_model.pb", "rb") as f:
-    graph_def = tf.GraphDef()
-    graph_def.ParseFromString(f.read())
+    data = compat.as_bytes(f.read());
+    sm = saved_model_pb2.SavedModel();
+    sm.ParseFromString(data);
+    
+    graph_def = sm.meta_graphs[0].graph_def;
     onnx_model = tensorflow_graph_to_onnx_model(graph_def,
                                      "dense_1/Softmax",
                                      opset=6)
