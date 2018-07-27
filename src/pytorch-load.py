5dd4abf6c31f6151a90d3ac28afcc48ff84a849e
Vegard Ekblad Itland <Vegard.Itland@student.uib.no>
2018-07-27 12:34:52 +0200

    Add PyTorch sample code (WIP)

---
+++ src/pytorch-load.py
@@ -0,0 +1,24 @@
+# based on: https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html
+from __future__ import print_function, division
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import numpy as np
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+import time
+import os
+import copy
+
+data_dir = 'hymenoptera_data'
+image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x),
+                                          data_transforms[x])
+                  for x in ['train', 'val']}
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4,
+                                             shuffle=True, num_workers=4)
+              for x in ['train', 'val']}
+dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
+class_names = image_datasets['train'].classesdevice = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
