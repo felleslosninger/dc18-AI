3930b41208a26c1c32c67bbe890b4321fee4b0cc
Viktor Andersen <vikfand@gmail.com>
2018-08-03 07:51:33 +0000

    Make python 2 compatible

--- src/stillinger-nav/lib/preprocessing.py
+++ src/stillinger-nav/lib/preprocessing.py
@@ -1,5 +1,8 @@
 import re
-import html
+import sys
+
+def is_version_3():
+    return sys.version_info == (3,)
 
 whitespace_pattern = re.compile(r'\s+')
 
@@ -9,7 +12,12 @@ def remove_tags(raw_html):
     return cleantext
 
 def unescape_html(raw_html):
-    return html.unescape(raw_html)
+    if is_version_3():
+        import html
+        return html.unescape(raw_html)
+    else:
+        import HTMLParser
+        return HTMLParser.HTMLParser().unescape(raw_html)
 
 def remove_whitespace(text):
     return whitespace_pattern.sub(' ', text)
